bc043ba64fb6700782e4d6b7b26a52e3a1f5e2d1
Naman Bhatt <65487375+NamanBhatt1234@users.noreply.github.com>
2022-01-13 13:14:36 +0530

    Pattern 3(Sheet 2)

--- 3.cc
+++ 3.cc
@@ -1,17 +1,19 @@
 #include<stdio.h>
+int fact(int n)
+{
+ if(n)
+    return n*fact(n-1);
+ else
+    return 1;
+}
 int main()
 {
  int i,j;
- for(i=0;i<5;i++)
-    {
-     for(j=0;j<5;j++)
-     {
-      if(j<i)
-         printf(" ");
-      else
-         printf("*");
-     }
-     printf("\n");
-    }
+ for(i=0;i<6;i++)
+ {
+  for(j=0;j<=i;j++)
+          printf("%d ",fact(i)/(fact(i-j)*fact(j)));
+  printf("\n");
+ }
  return 0;
 }
