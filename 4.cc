e7f291e88e6654ccb606c757da08d9e592f91c8e
Naman Bhatt <65487375+NamanBhatt1234@users.noreply.github.com>
2022-01-13 13:15:07 +0530

    Pattern 4(Sheet 2)

--- 4.cc
+++ 4.cc
@@ -1,26 +1,27 @@
 #include<stdio.h>
 int main()
 {
- int i,j;
- for(i=0;i<5;i++)
- {
-  for(j=0;j<9;j++)
-        if(j<=4+i)
-          {
-           if(j<4-i)
-              printf(" ");
-           else if((i+1)%2)
-               if(j%2)
-                printf(" ");
-               else
+   int i,j;
+   for(i=0;i<9;i++)
+     {
+        if(i<=4)
+        {
+          for(j=0;j<9;j++)
+             if(j<i)
+               printf(" ");
+             else if(j<=2*i)
                 printf("*");
-           else
-               if(j%2)
-                printf("*");
-               else
+          printf("\n");
+        }
+        else
+         {
+          for(j=0;j<9;j++)
+             if(j<8-i)
                 printf(" ");
-          }
-  printf("\n");
- }
+             else if(j<=2*(8-i))
+                printf("*");
+          printf("\n");
+         }
+     }
  return 0;
 }
